cf2bcb13c40222528e998d795e3c3d8fe1ed34d2
James <1102625902@qq.com>
2023-08-17 16:46:09 +0800

    create project  with app factory

---
+++ blog_app/__init__.py
@@ -0,0 +1,28 @@
+"""
+    应用工厂创建Flask实例，完成实例的配置，返回实例app
+"""
+
+from flask import Flask
+import os
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    return app
